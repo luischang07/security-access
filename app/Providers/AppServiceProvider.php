a996bec84606b66fd04e84a35666fdca15c8c27e
Anita033 <L21170417@culiacan.tecnm.mx>
2025-11-27 20:39:28 -0700

    MErequetengue 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,5 +18,9 @@ class AppServiceProvider extends ServiceProvider
   public function boot(): void
   {
     // El throttling se maneja en la base de datos
+  }
+  public function boot()
+  {
+    Schema::defaultStringLength(191);
   }
 }
