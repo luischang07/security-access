baba499b2e910744d712164193cbb9f482daed66
Fernando Chiquete <ferchiquete2003@gmail.com>
2025-11-28 19:46:00 -0700

    Reapply "Error vista2"
    This reverts commit 214c3ebd249cce97fea5e501607cfae9c7094fab.

--- app/Services/Modelos/PedidoService.php
+++ app/Services/Modelos/PedidoService.php
@@ -111,7 +111,7 @@ class PedidoService
     {
         return $this->dataBase->obtenerPedidoPorId($pedido_id);
     }
-    public function setCedulaProfesional($cedula, $pedido)
+    public function setCedulaProfesional($cedula, Pedido $pedido)
     {
         $pedido->setCedulaProfesional($cedula);
         return $pedido;
